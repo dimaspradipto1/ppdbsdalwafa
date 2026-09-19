a46fb4767ac896e76c9911c8ce284243d5b36b43
dimas pradipto <dpradipto8@gmail.com>
2026-09-19 09:43:19 +0700

    feat: implement authentication system with role-based middleware and login view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::controller(AuthController::class)->group(function () {
+    Route::get('/', 'login')->name('login');
+    Route::post('/logout', 'logout')->name('logout');
 });
